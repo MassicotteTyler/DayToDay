c3e756bb91c7e7d1c7aeccd2f777c71d4f71e24c
Tyler Massicotte <Tyler@pseudocube.com>
2024-11-03 22:10:44 +0000

    Merged PR 11: Wallet Component
    Added a Wallet Component.
    
    #Events
    * OnBalanceChange - Triggered when the _balance is modified
    * OnWithdrawal
    * OnDeposit
    
    Related work items: #57

---
+++ Assets/Scripts/Component/WalletComponent.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Component
+{
+    /// <summary>
+    /// Component that can be attached to a GameObject to give it a wallet.
+    /// </summary>
+    public class WalletComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// Event that is triggered when the balance of the wallet changes.
+        /// </summary>
+        public UnityEvent OnBalanceChanged;
+
+        /// <summary>
+        /// Event that is triggered when money is withdrawn from the wallet.
+        /// </summary>
+        public UnityEvent<float> OnWithdrawal;
+        
+        /// <summary>
+        /// Event that is triggered when money is deposited into the wallet.
+        /// </summary>
+        public UnityEvent<float> OnDeposit;
+        
+        /// <summary>
+        /// The current balance of the wallet.
+        /// </summary>
+        private float _balance = 0;
+        
+        /// <summary>
+        /// Public accessor for the balance of the wallet.
+        /// </summary>
+        public float Balance
+        {
+            get => _balance;
+            // Private set that triggers event
+            private set
+            {
+                _balance = value;
+                OnBalanceChanged?.Invoke();
+            }
+        }
+        
+        /// <summary>
+        /// Deposit money into the wallet.
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(float amount)
+        {
+            Balance += amount;
+            OnDeposit?.Invoke(amount);
+        }
+        
+        /// <summary>
+        /// Withdraw money from the wallet.
+        /// </summary>
+        /// <param name="amount">Amount to withdraw</param>
+        public void Withdraw(float amount)
+        {
+            if (!CanAfford(amount)) return;
+            
+            Balance = Math.Clamp(Balance - amount, 0, _balance);
+            OnWithdrawal?.Invoke(amount);
+        }
+        
+        /// <summary>
+        /// Check if the wallet can afford a certain amount.
+        /// </summary>
+        /// <param name="amount">The amount to check</param>
+        /// <returns>If the wallet balance is higher than the requested amount</returns>
+        public bool CanAfford(float amount)
+        {
+            return Balance >= amount;
+        }
+    }
+}
