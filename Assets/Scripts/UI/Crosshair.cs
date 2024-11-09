0172fa9ac049ee1324a2e9253568dcda38a2e01b
Tyler Massicotte <Tyler@pseudocube.com>
2024-11-09 09:06:14 +0000

    Merged PR 35: added crosshair
    added crosshair
    
    ![image.png](https://dev.azure.com/PseudoGameDev/94b8dee8-fd91-4480-a43d-5ca6792c93e7/_apis/git/repositories/84426cfc-8d1e-4ec2-ad41-c5c8451902d5/pullRequests/35/attachments/image.png)
    
    Related work items: #71

---
+++ Assets/Scripts/UI/Crosshair.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    /// <summary>
+    /// Component that displays the crosshair on the screen.
+    /// </summary>
+    public class Crosshair : MonoBehaviour
+    {
+        
+    }
+}
