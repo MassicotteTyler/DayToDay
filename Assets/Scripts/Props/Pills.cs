395bf35cf7ac55f4a092352c0d1950f4f4a2ef7f
Tyler Massicotte <Tyler@pseudocube.com>
2024-11-09 08:45:48 +0000

    Merged PR 32: added pills
    added pills
    
    Related work items: #74

---
+++ Assets/Scripts/Props/Pills.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Component;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Props
+{
+    [RequireComponent(typeof(InteractableComponent))]
+    public class Pills : MonoBehaviour
+    {
+       private InteractableComponent _interactableComponent;
+
+       private void Start()
+       {
+              _interactableComponent = GetComponent<InteractableComponent>();
+              _interactableComponent.onInteract?.AddListener(Interact);
+       }
+       
+       public void Interact(GameObject interactor)
+       {
+              Debug.Log("Interacting with pills");
+       }
+    }
+}
