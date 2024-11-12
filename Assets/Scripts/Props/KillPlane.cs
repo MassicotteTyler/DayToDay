9cc667ab5d18af9010c16169eb4afb3224443ee2
Tyler Massicotte <Tyler@pseudocube.com>
2024-11-12 23:55:07 +0000

    Merged PR 47: added kill plane
    Collision that reloads the current scene when collided with. Eventually will be event driven so we can have transition effects
    
    Related work items: #95

---
+++ Assets/Scripts/Props/KillPlane.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SceneManagement;
+using UnityEngine;
+
+namespace Props
+{
+    /// <summary>
+    /// Kill plane that reloads the scene when the player falls off the map.
+    /// </summary>
+    [RequireComponent(typeof(BoxCollider))]
+    public class KillPlane : MonoBehaviour
+    {
+        /// <summary>
+        /// The BoxCollider attached to this GameObject.
+        /// </summary>
+        private BoxCollider _boxCollider;
+
+        private void Awake()
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+            _boxCollider.isTrigger = true;
+        }
+
+        /// <summary>
+        /// Reload the scene when the player falls off the map.
+        /// </summary>
+        /// <param name="other"></param>
+        private void OnTriggerEnter(Collider other)
+        {
+            // Check if player
+            if (other.CompareTag("Player"))
+            {
+                Bootstrapper.Instance.SceneLoader.ReloadActiveSceneGroup();
+            }
+        }
+    }
+}
